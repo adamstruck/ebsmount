4fb2baa59cd953c1e84f532e979a30c3cc88f48a
adamstruck <strucka@ohsu.edu>
2018-03-09 19:52:48 -0800

    added more resources

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
-FROM centos:7.4.1708
-RUN yum install -y e4fsprogs curl docker
-RUN systemctl enable docker
+FROM alpine
+RUN apk add --update --no-cache curl jq docker
 ADD ebsmount /usr/local/bin/ebsmount
+# add a custom script that will make requests to a ebsmount server
+# ADD ./mountAndStartWorker.sh /opt/mountAndStartWorker.sh
+CMD ["sh"]
---
+++ resources/funnel/mountAndStartWorker.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+set -e
+set -o pipefail
+
+# check required env vars are set
+[ -z "$DISK" ] && echo "Need to set DISK"
+[ -z "$TASKID" ] && echo "Need to set TASKID"
+([ -z "$TASKID" ] || [ -z "$TASKID" ]) && exit 1 
+
+set -o xtrace
+
+echo "mounting $DISK GB EBS volume to /mnt/$TASKID..."
+curl -d "{\"Size\": $DISK, \"MountPoint\": \"/mnt/$TASKID\", \"VolumeType\": \"gp2\", \"FSType\": \"ext4\"}" \
+     --unix-socket /var/run/ebsmount.sock \
+     http://localhost/mount \
+     > vid
+VID=$(cat vid | jq -r .VolumeID)
+
+echo "starting funnel worker..."
+docker run -i \
+       -v /var/run/docker.sock:/var/run/docker.sock \
+       -v /mnt:/mnt \
+       docker.io/ohsucompbio/funnel:kf-dev worker \
+       run \
+       --Worker.WorkDir /mnt \
+       --Database dynamodb \
+       --DynamoDB.Region us-east-1 \
+       --DynamoDB.TableBasename funnel \
+       --Worker.PollingRate 30s \
+       --Worker.LogUpdateRate 10m \
+       --Worker.LogTailSize 10000 \
+       --taskID $TASKID
+
+echo "unmounting and deleting EBS volume $VID..."
+curl -d "{\"VolumeID\": \"$VID\", \"MountPoint\": \"/mnt/$TASKID\"}" \
+     --unix-socket /var/run/ebsmount.sock \
+     http://localhost/unmount
